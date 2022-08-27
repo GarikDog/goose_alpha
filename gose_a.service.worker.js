432e1b1c1efb2235700edd04e31c63c44e9af0ab
Igor <sobakapppoe@gmail.com>
2022-08-27 03:57:28 +0300

    v0.01

--- gose_a.service.worker.js
+++ gose_a.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1661561405|356383736";
+const CACHE_VERSION = "1661561808|759613012";
 const CACHE_PREFIX = "2d_platformer_pr-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "gose_a.offline.html";
